5d1e8f541146ed146cfa53af9dc5dfe70b20a8cf
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-01-19 11:22:41 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,11 +8,21 @@ const langflowRouter = require("./routes/langflowRoute");
 const homeRouter = require("./routes/home");
 const chatRouter = require("./routes/chatRoute");
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'https://teamlogix.web.app',
+    ];
+    if (!origin || !allowedOrigins.includes(origin)) {
+      callback(new Error('Not allowed by CORS'));
+    } else {
+      callback(null, true);
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
